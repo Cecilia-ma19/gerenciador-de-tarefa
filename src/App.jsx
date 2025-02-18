c26996f66d3a2f3c3ee161cefc7da88565b96dc7
Cecília Maria <cecilia.msaraujo9@gmail.com>
2025-02-18 18:35:58 -0300

    adicionado seção de tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
+import Tasks from "./components/Tasks";
 import { useState } from "react";
+import AddTask from "./components/AddTask";
 
 function App() {
-  const [message, setMesasse] = useState("Olá, mundo");
-
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description:
+        "Estudar programação para se tornar um grande desenvolvedor full stack.",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar inglês",
+      description: "Estudar inglês para se tornar fluente",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar matematica",
+      description:
+        "Estudar matemática para se tornar um grande desenvolvedor full stack.",
+      isCompleted: false,
+    },
+  ]);
   return (
-    <div>
-      <h1>{message}</h1>
-      <button
-        onClick={() => {
-          setMesasse("Olá, fui clicado!");
-        }}
-      >
-        Mudar mensagem
-      </button>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks tasks={tasks} />
+      </div>
     </div>
   );
 }
